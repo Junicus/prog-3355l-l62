6621a089db603755eb44a5c105a29806be77d1cb
Junicus <junicus@outlook.com>
2023-04-12 14:45:31 -0400

    codigo de frmMain

---
+++ src/Lab62/frmMain.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Lab62;
+
+partial class frmMain
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbTarea = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbCalificacionRange = new System.Windows.Forms.ComboBox();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.lvData = new System.Windows.Forms.ListView();
+            this.chFecha = new System.Windows.Forms.ColumnHeader();
+            this.chEstudiante = new System.Windows.Forms.ColumnHeader();
+            this.chActividad = new System.Windows.Forms.ColumnHeader();
+            this.chPuntuacion = new System.Windows.Forms.ColumnHeader();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lvData, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(897, 311);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.AutoSize = true;
+            this.tableLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel2.ColumnCount = 7;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.cbTarea, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.cbCalificacionRange, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnCargar, 5, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnLimpiar, 6, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(891, 45);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tarea:";
+            // 
+            // cbTarea
+            // 
+            this.cbTarea.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbTarea.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cbTarea.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cbTarea.FormattingEnabled = true;
+            this.cbTarea.Location = new System.Drawing.Point(50, 11);
+            this.cbTarea.Name = "cbTarea";
+            this.cbTarea.Size = new System.Drawing.Size(162, 23);
+            this.cbTarea.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(220, 15);
+            this.label2.Margin = new System.Windows.Forms.Padding(5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Rango Calificaciones:";
+            // 
+            // cbCalificacionRange
+            // 
+            this.cbCalificacionRange.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbCalificacionRange.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cbCalificacionRange.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cbCalificacionRange.FormattingEnabled = true;
+            this.cbCalificacionRange.Location = new System.Drawing.Point(348, 11);
+            this.cbCalificacionRange.Name = "cbCalificacionRange";
+            this.cbCalificacionRange.Size = new System.Drawing.Size(162, 23);
+            this.cbCalificacionRange.TabIndex = 3;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCargar.AutoSize = true;
+            this.btnCargar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnCargar.Location = new System.Drawing.Point(558, 5);
+            this.btnCargar.Margin = new System.Windows.Forms.Padding(5);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Padding = new System.Windows.Forms.Padding(5);
+            this.btnCargar.Size = new System.Drawing.Size(158, 35);
+            this.btnCargar.TabIndex = 4;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLimpiar.AutoSize = true;
+            this.btnLimpiar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnLimpiar.Location = new System.Drawing.Point(726, 5);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(5);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Padding = new System.Windows.Forms.Padding(5);
+            this.btnLimpiar.Size = new System.Drawing.Size(160, 35);
+            this.btnLimpiar.TabIndex = 5;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // lvData
+            // 
+            this.lvData.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chFecha,
+            this.chEstudiante,
+            this.chActividad,
+            this.chPuntuacion});
+            this.lvData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvData.Location = new System.Drawing.Point(3, 54);
+            this.lvData.Name = "lvData";
+            this.lvData.Size = new System.Drawing.Size(891, 254);
+            this.lvData.TabIndex = 1;
+            this.lvData.UseCompatibleStateImageBehavior = false;
+            this.lvData.View = System.Windows.Forms.View.Details;
+            // 
+            // chFecha
+            // 
+            this.chFecha.Text = "Fecha";
+            this.chFecha.Width = 160;
+            // 
+            // chEstudiante
+            // 
+            this.chEstudiante.Text = "Estudiante";
+            this.chEstudiante.Width = 240;
+            // 
+            // chActividad
+            // 
+            this.chActividad.Text = "Actividad";
+            this.chActividad.Width = 160;
+            // 
+            // chPuntuacion
+            // 
+            this.chPuntuacion.Text = "Puntuacion";
+            this.chPuntuacion.Width = 160;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 311);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(900, 350);
+            this.Name = "frmMain";
+            this.Text = "Puntuaciones";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private TableLayoutPanel tableLayoutPanel1;
+    private TableLayoutPanel tableLayoutPanel2;
+    private Label label1;
+    private ComboBox cbTarea;
+    private Label label2;
+    private ComboBox cbCalificacionRange;
+    private Button btnCargar;
+    private Button btnLimpiar;
+    private ListView lvData;
+    private ColumnHeader chFecha;
+    private ColumnHeader chEstudiante;
+    private ColumnHeader chActividad;
+    private ColumnHeader chPuntuacion;
+}
